50d0cf5e85f04765dcae7b84c2a5c6ac7ba5aebf
unknown <rezaaami09@gmail.com>
2026-08-26 07:40:05 +0700

    memisahkan codingan js

--- script.js
+++
@@ -1,8 +0,0 @@
-console.log("DailyBoard siap dijalankan")
-
-const app = document.getElementById("app")
-
-// Membuat Kerangka Website
-const judul = document.createElement("h2");
-judul.textContent = "Selamat Datang di DailyBoard";
-app.appendChild(judul);
